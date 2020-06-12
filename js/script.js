3e4b1ed84c822bfaf4fd2f0ec5dd25177e019896
Andrea <miranda.andrea.nd@gmail.com>
2020-06-12 18:23:49 +0200

    finito

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// chiedere all'utente il suo nome
+var nome = prompt ("inserisci il tuo nome:");
+
+//chiedere il cognome
+var cognome = prompt ("inserisci il tuo cognome:");
+
+// chidere il colore preferito
+var colore = prompt ("inserisci il tuo colore preferito")
+
+
+
+
+
+
+
+
+
+
+document.getElementById('user').innerHTML = "grazie " + nome + cognome + colore+"19";
